fb87f5ffaef75e4dc6450d73254717522cbdd660
Nicolas Tang <nico9156@gmail.com>
2025-03-05 20:57:46 -0500

    add argocd

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -17,6 +17,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+      pkgs.argocd
       pkgs.bat
       pkgs.direnv
       pkgs.coreutils
